986e4c7465614b898f30710bdc9db1ce79668bf3
pogosyanserg <45202538+pogosyanserg@users.noreply.github.com>
2018-11-20 16:04:54 +0300

    Add files via upload

---
+++ Document1.py
@@ -0,0 +1,40 @@
+import requests
+from time import sleep
+import datetime
+
+greet_bot = BotHandler(token)  
+greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
+now = datetime.datetime.now()
+
+url = "https://api.telegram.org/bot692896424:AAGuZJutnqyhxaMcQPRl1N9vzoiyh5s9Id0/"
+
+def get_updates_json(request):  
+    params = {'timeout': 100, 'offset': None}
+    response = requests.get(request + 'getUpdates', data=params)
+    return response.json()
+
+
+def last_update(data):  
+    results = data['result']
+    total_updates = len(results) - 1
+    return results[total_updates]
+	
+	def get_chat_id(update):  
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+def send_mess(chat, text):  
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+def main():  
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
+           update_id += 1
+        sleep(1)       
+
+if __name__ == '__main__':  
+    main()
